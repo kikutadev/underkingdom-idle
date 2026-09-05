a96d0c67443277b6c9eecc7a6d7b12111a165e85
kikutadev <kikutadev@users.noreply.github.com>
2026-09-05 21:11:17 +0900

    deploy: Underkingdom Idle 6151392b627d

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = 'cae21818e5a9';
+const CACHE_VERSION = '6151392b627d';
 /** @type {string} */
 const CACHE_PREFIX = 'Underkingdom Idl-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
@@ -17,7 +17,7 @@ const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = false;
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["index.wasm","index-cae21818e5a9.pck"];
+const CACHEABLE_FILES = ["index.wasm","index-6151392b627d.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
