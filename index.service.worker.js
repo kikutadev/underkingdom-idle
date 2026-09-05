9158d23a0ef43d868385b3cc1be960e57dabdac4
kikutadev <kikutadev@users.noreply.github.com>
2026-09-05 14:06:22 +0900

    deploy: Underkingdom Idle 8153302d0bfc

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788583605|29515878';
+const CACHE_VERSION = '1788584739|46717235';
 /** @type {string} */
 const CACHE_PREFIX = 'Underkingdom Idl-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
